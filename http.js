c767913cea5cdcaf779188e2bc4b1310104362f0
nekofurball <24914325+nekofurball@users.noreply.github.com>
2019-07-18 18:48:40 +1000

    First patch.
    If a user tried to connect to the server over HTTP(S) it would crash as an incorrect variable pointer was specified under line 7 col 95.
    
    Tested. No longer crashes.

--- http.js
+++ http.js
@@ -4,7 +4,7 @@ const motd = fs.readFileSync(__dirname + "/motd.txt", "utf-8");
 function handler(req, res) {
   res.writeHead(400);
   res.send("<h1>You are connecting to a Trollbox++ server over HTTP(S)!</h1>Here is this server's MOTD:\n<pre>" +
-    motd.replace(/{{{{{GIBEHOSTPLS}}}}}/g,   "ws" + (config.tbpp.https ? "s" : "") + "://" + (conn.handshake.headers.host || "localhost:" + port) + "/") +
+    motd.replace(/{{{{{GIBEHOSTPLS}}}}}/g,   "ws" + (config.tbpp.https ? "s" : "") + "://" + (req.headers.host || "localhost:" + port) + "/") +
     "\n</pre>");
 }
 
